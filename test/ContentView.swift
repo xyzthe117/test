4813c4dbb5650a61d7bf0566a298ba769f89d788
xyzthe117 <xyzthe117@gmail.com>
2020-06-16 03:25:28 +0700

    Initial Commit

---
+++ test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by Thanathkitt Paetyathai on 16/6/2563 BE.
+//  Copyright © 2563 qrkx. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
